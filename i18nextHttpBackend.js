01fe94637d7a1388c91a7889d599e52b731908df
Adriano Raiano <adriano@raiano.ch>
2020-04-14 21:40:44 +0200

    fix for bundlers

--- i18nextHttpBackend.js
+++ i18nextHttpBackend.js
@@ -1,4 +1,12 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.i18nextHttpBackend = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+if (typeof require !== 'undefined') {
+  var f = require('node-fetch')
+  if (f.default) f = f.default
+  exports.default = f
+  module.exports = exports.default
+}
+
+},{"node-fetch":5}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -196,7 +204,7 @@ Backend.type = 'backend';
 var _default = Backend;
 exports["default"] = _default;
 module.exports = exports.default;
-},{"./request.js":2,"./utils.js":3}],2:[function(require,module,exports){
+},{"./request.js":3,"./utils.js":4}],3:[function(require,module,exports){
 (function (global){
 "use strict";
 
@@ -204,6 +212,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
+
+var fetchNode = _interopRequireWildcard(require("./getFetch.cjs"));
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
@@ -246,6 +256,8 @@ if (typeof ActiveXObject === 'function') {
     ActiveXObjectApi = window.ActiveXObject;
   }
 }
+
+if (!fetchApi && fetchNode) fetchApi = fetchNode["default"] || fetchNode; // because of strange export
 
 var addQueryString = function addQueryString(url, params) {
   if (params && _typeof(params) === 'object') {
@@ -363,22 +375,18 @@ var request = function request(options, url, payload, callback) {
   if (typeof XMLHttpRequest === 'function' || typeof ActiveXObject === 'function') {
     // use xml http request
     return requestWithXmlHttpRequest(options, url, payload, callback);
-  }
+  } // import('node-fetch').then((fetch) => {
+  //   fetchApi = fetch.default || fetch // because of strange export of node-fetch
+  //   requestWithFetch(options, url, payload, callback)
+  // }).catch(callback)
 
-  Promise.resolve().then(function () {
-    return _interopRequireWildcard(require('node-fetch'));
-  }).then(function (fetch) {
-    fetchApi = fetch["default"] || fetch; // because of strange export of node-fetch
-
-    requestWithFetch(options, url, payload, callback);
-  })["catch"](callback);
 };
 
 var _default = request;
 exports["default"] = _default;
 module.exports = exports.default;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"node-fetch":4}],3:[function(require,module,exports){
+},{"./getFetch.cjs":1}],4:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -399,7 +407,7 @@ function defaults(obj) {
   });
   return obj;
 }
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 (function (global){
 "use strict";
 
@@ -425,5 +433,5 @@ exports.Headers = global.Headers;
 exports.Request = global.Request;
 exports.Response = global.Response;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[1])(1)
+},{}]},{},[2])(2)
 });
