d41a46bbf318d783647eb4636112f5fac2172495
Adriano Raiano <adriano@raiano.ch>
2020-04-19 09:07:26 +0200

    node example

---
+++ example/node/app.js
@@ -0,0 +1,23 @@
+// serve translations
+const express = require('express')
+const app = express()
+app.use('/locales', express.static('locales'))
+app.listen(8080)
+
+// i18next in action...
+const i18next = require('i18next')
+const HttpBackend = require('i18next-http-backend')
+i18next.use(HttpBackend).init({
+  lng: 'en',
+  fallbackLng: 'en',
+  preload: ['en', 'de'],
+  ns: ['translation'],
+  defaultNS: 'translation',
+  backend: {
+    loadPath: 'http://localhost:8080/locales/{{lng}}/{{ns}}.json'
+  }
+}, (err, t) => {
+  if (err) return console.error(err)
+  console.log(t('welcome'))
+  console.log(t('welcome', { lng: 'de' }))
+})
