fd67a52ed37f15831a21280d56ccc6b3976e002f
Adriano Raiano <adriano@raiano.ch>
2020-04-24 14:21:49 +0200

    fix for react-natice fixes #5

--- i18nextHttpBackend.js
+++ i18nextHttpBackend.js
@@ -1,11 +1,22 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.i18nextHttpBackend = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function (global){
+var fetchApi
+if (typeof fetch === 'function') {
+  if (typeof global !== 'undefined' && global.fetch) {
+    fetchApi = global.fetch
+  } else if (typeof window !== 'undefined' && window.fetch) {
+    fetchApi = window.fetch
+  }
+}
+
 if (typeof require !== 'undefined') {
-  var f = require('node-fetch')
+  var f = fetchApi || require('node-fetch')
   if (f.default) f = f.default
   exports.default = f
   module.exports = exports.default
 }
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"node-fetch":5}],2:[function(require,module,exports){
 "use strict";
 
@@ -217,7 +228,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var fetchNode = _interopRequireWildcard(require("./getFetch.cjs"));
+var fetchNode = _interopRequireWildcard(require("./getFetch.js"));
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
@@ -389,8 +400,9 @@ var request = function request(options, url, payload, callback) {
 var _default = request;
 exports["default"] = _default;
 module.exports = exports.default;
+
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./getFetch.cjs":1}],4:[function(require,module,exports){
+},{"./getFetch.js":1}],4:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
