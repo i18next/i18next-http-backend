c48f7a0e509ac1241ea10050f8298425d574c645
Adriano Raiano <adriano@raiano.ch>
2020-05-29 17:47:35 +0200

    fix for non-fetch browsers using i18nextHttpBackend.js

--- i18nextHttpBackend.js
+++ i18nextHttpBackend.js
@@ -270,6 +270,8 @@ if (typeof ActiveXObject === 'function') {
 
 if (!fetchApi && fetchNode) fetchApi = fetchNode["default"] || fetchNode; // because of strange export
 
+if (typeof fetchApi !== 'function') fetchApi = undefined;
+
 var addQueryString = function addQueryString(url, params) {
   if (params && _typeof(params) === 'object') {
     var queryString = ''; // Must encode data
@@ -418,30 +420,6 @@ function defaults(obj) {
   return obj;
 }
 },{}],5:[function(require,module,exports){
-(function (global){
-"use strict";
 
-// ref: https://github.com/tc39/proposal-global
-var getGlobal = function () {
-	// the only reliable means to get the global object is
-	// `Function('return this')()`
-	// However, this causes CSP violations in Chrome apps.
-	if (typeof self !== 'undefined') { return self; }
-	if (typeof window !== 'undefined') { return window; }
-	if (typeof global !== 'undefined') { return global; }
-	throw new Error('unable to locate global object');
-}
-
-var global = getGlobal();
-
-module.exports = exports = global.fetch;
-
-// Needed for TypeScript and Webpack.
-exports.default = global.fetch.bind(global);
-
-exports.Headers = global.Headers;
-exports.Request = global.Request;
-exports.Response = global.Response;
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[2])(2)
 });
