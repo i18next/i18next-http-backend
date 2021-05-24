42c024bd041ab00860a7cd14457c2e14e4811c3b
Adriano Raiano <adriano@raiano.ch>
2021-05-24 13:05:30 +0200

    update fallback example

--- example/fallback/app.js
+++ example/fallback/app.js
@@ -2,34 +2,20 @@
 const express = require('express')
 const app = express()
 app.use('/locales', express.static('locales'))
-app.listen(8080)
+const server = app.listen(8080)
+
+const resourcesToBackend = require('i18next-resources-to-backend')
 
 const localResources = {
   en: {
-    translationFallback: {
-      welcome: {
-        from: {
-          fallback: 'hello world from local fallback'
-        }
-      }
+    translation: {
+      welcome: 'hello world from local fallback'
     }
   },
   de: {
-    translationFallback: {
-      welcome: {
-        from: {
-          fallback: 'hallo welt vom lokalen fallback'
-        }
-      }
+    translation: {
+      welcome: 'hallo welt vom lokalen fallback'
     }
-  }
-}
-
-const customLocalBackend = {
-  type: 'backend',
-  init: function(services, backendOptions, i18nextOptions) { /* use services and options */ },
-  read: function(language, namespace, callback) {
-    callback(null, localResources[language][namespace]);
   }
 }
 
@@ -38,26 +24,39 @@ const i18next = require('i18next')
 const ChainedBackend = require('i18next-chained-backend')
 const HttpBackend = require('i18next-http-backend')
 // const HttpBackend = require('../../cjs')
-i18next.use(ChainedBackend).init({
-  debug: true,
-  lng: 'en',
-  fallbackLng: 'en',
-  preload: ['en', 'de'],
-  ns: ['translation', 'translationFallback'],
-  defaultNS: 'translation',
-  backend: {
-    backends: [
-      HttpBackend, // if a namespace can't be loaded via normal http-backend loadPath, then the customLocalBackend will try to return the correct resources
-      customLocalBackend
-    ],
-    backendOptions: [{
-      loadPath: 'http://localhost:8080/locales/{{lng}}/{{ns}}.json'
-    }, {}]
-  }
-}, (err, t) => {
+
+const initI18next = (cb) => {
+  const i18n = i18next.createInstance()
+  i18n.use(ChainedBackend).init({
+    // debug: true,
+    lng: 'en',
+    fallbackLng: 'en',
+    preload: ['en', 'de'],
+    ns: ['translation'],
+    defaultNS: 'translation',
+    backend: {
+      backends: [
+        HttpBackend, // if a namespace can't be loaded via normal http-backend loadPath, then the inMemoryLocalBackend will try to return the correct resources
+        resourcesToBackend(localResources)
+      ],
+      backendOptions: [{
+        loadPath: 'http://localhost:8080/locales/{{lng}}/{{ns}}.json'
+      }]
+    }
+  }, cb)
+}
+
+initI18next((err, t) => {
   if (err) return console.error(err)
   console.log(t('welcome'))
   console.log(t('welcome', { lng: 'de' }))
-  console.log(t('welcome.from.fallback', { ns: 'translationFallback' }))
-  console.log(t('welcome.from.fallback', { lng: 'de', ns: 'translationFallback' }))
+  console.log('stopping http server...')
+  server.close(() => {
+    console.log('http server stopped')
+    initI18next((err, t) => {
+      if (err) return console.error(err)
+      console.log(t('welcome'))
+      console.log(t('welcome', { lng: 'de' }))
+    })
+  })
 })
